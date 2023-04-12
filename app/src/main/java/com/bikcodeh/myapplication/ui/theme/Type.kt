9350a62c0af33af1bc663da04f0c5efe6e9b4b36
Bikcodeh <bikcodeh@gmail.com>
2023-04-12 13:19:03 -0500

    add fonts, colors

--- app/src/main/java/com/bikcodeh/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/bikcodeh/myapplication/ui/theme/Type.kt
@@ -2,33 +2,110 @@ package com.bikcodeh.myapplication.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.bikcodeh.myapplication.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val WeatherFonts = FontFamily(
+    Font(R.font.nunito_light, weight = FontWeight.Light),
+    Font(R.font.nunito_medium, weight = FontWeight.Medium),
+    Font(R.font.nunito_regular, weight = FontWeight.Normal),
+    Font(R.font.nunito_bold, weight = FontWeight.Bold)
+)
+
+val WeatherTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = WeatherFonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 57.sp,
+        letterSpacing = 0.em,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 45.sp,
+        letterSpacing = 0.em,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        letterSpacing = 0.em,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        letterSpacing = 0.01.em,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        letterSpacing = 0.em,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp,
+        letterSpacing = 0.15.em,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = WeatherFonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 20.sp,
+        letterSpacing = 0.01.em
+    ),
+    titleMedium = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.em,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.09.em
+
+    ),
+    labelLarge = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        letterSpacing = 0.02.em
+    ),
+    labelMedium = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        letterSpacing = 0.em
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = WeatherFonts,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.em
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        letterSpacing = 0.03.em
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.em
+    ),
+    bodySmall = TextStyle(
+        fontFamily = WeatherFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.02.em
     )
-    */
 )
