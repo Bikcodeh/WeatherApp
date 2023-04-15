13fe55a7fe705825609c066a62954393f25fee67
Bikcodeh <bikcodeh@gmail.com>
2023-04-15 12:28:59 -0500

    create network module and add dependencies like retrofit, moshi and so on

---
+++ app/src/main/java/com/bikcodeh/myapplication/data/remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,23 @@
+package com.bikcodeh.myapplication.data.remote.interceptor
+
+import com.bikcodeh.myapplication.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor: Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val newUrl = originalRequest.url.newBuilder()
+            .addQueryParameter(QUERY_API_KEY, BuildConfig.WEATHER_API)
+            .build()
+        val newRequest = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+    companion object {
+        const val QUERY_API_KEY = "appid"
+    }
+}
---
+++ app/src/main/java/com/bikcodeh/myapplication/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.bikcodeh.myapplication.di
+
+import com.bikcodeh.myapplication.BuildConfig
+import com.bikcodeh.myapplication.data.remote.interceptor.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            retryOnConnectionFailure(true)
+            addInterceptor(AuthInterceptor())
+            addInterceptor(loggingInterceptorProvider())
+        }.build()
+    }
+
+    private fun loggingInterceptorProvider(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(
+            if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        okHttpClient: OkHttpClient
+    ) = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+}
+
+private const val BASE_URL = "https://api.openweathermap.org/data/2.5/forecast/"
+private const val CONNECT_TIMEOUT = 15L
+private const val WRITE_TIMEOUT = 15L
+private const val READ_TIMEOUT = 15L
